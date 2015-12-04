5b0b9702485f238042557a20ba3a34625ee10574
lifeqiuzhi520 <1790947862@qq.com>
2015-12-04 22:04:57 +0800

    相册主页面布局实现

---
+++ main/java/com/camera/www/camera/BaseAct.java
@@ -0,0 +1,15 @@
+package com.camera.www.camera;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2015/12/4.
+ * we need know that 十二月
+ */
+public class BaseAct extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ main/java/com/camera/www/camera/camera/TakePictureAct.java
@@ -0,0 +1,12 @@
+package com.camera.www.camera.camera;
+
+import com.camera.www.camera.BaseAct;
+
+/**
+ * QQ:1790947862
+ * Created by xiezh on 2015/12/4.
+ * we need know that 十二月
+ */
+public class TakePictureAct extends BaseAct {
+
+}
